b183c853c9e92706df1c5d6cff4f923aee259238
Avani Tanwar <avanisoam@gmail.com>
2024-05-24 21:23:07 +0200

    Sample Navigation Drawer Added on HomeScreen

--- app/src/main/java/com/example/nordicnews/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/nordicnews/ui/home/HomeScreen.kt
@@ -4,6 +4,7 @@ import android.net.Uri
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -11,23 +12,42 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Info
+import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.NavigationDrawerItemDefaults
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
@@ -54,6 +74,7 @@ import com.example.nordicnews.ui.shared.FixedHeader
 import com.example.nordicnews.ui.shared.HorizontalCardListWithText
 import com.example.nordicnews.ui.theme.NordicNewsTheme
 import com.google.gson.Gson
+import kotlinx.coroutines.launch
 
 
 object HomeDestination : NavigationDestination {
@@ -61,6 +82,13 @@ object HomeDestination : NavigationDestination {
     override val titleRes = R.string.home
     override val iconVector = Icons.Filled.Home
 }
+
+data class NavigationItem(
+    val title: String,
+    val selectedIcon: ImageVector,
+    val unselectedIcon: ImageVector,
+    val badgeCount: Int? = null
+)
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(
@@ -71,111 +99,187 @@ fun HomeScreen(
     viewModel: HomeViewModel = viewModel(factory = HomeViewModel.Factory)
 ) {
     val uiState by viewModel.uiState.collectAsState()
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                colors = topAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.primaryContainer,
-                    titleContentColor = MaterialTheme.colorScheme.primary,
-                ),
-                title = {
-                    Text(stringResource(id = HomeDestination.titleRes))
-                }
-            )
-        },
-        bottomBar = {
-            BottomAppBar(
-                containerColor = Color.White,//MaterialTheme.colorScheme.primaryContainer,
-                //contentColor = Color.Yellow,//MaterialTheme.colorScheme.primary,
-            ) {
-                BottomNavigationBar(navController)
-            }
-        },
-        floatingActionButton = {
-            FloatingActionButton(onClick = {
+    val items = listOf(
+        NavigationItem(
+            title = "All",
+            selectedIcon = Icons.Filled.Home,
+            unselectedIcon = Icons.Outlined.Home,
+        ),
+        NavigationItem(
+            title = "Urgent",
+            selectedIcon = Icons.Filled.Info,
+            unselectedIcon = Icons.Outlined.Info,
+            badgeCount = 45
+        ),
+        NavigationItem(
+            title = "Settings",
+            selectedIcon = Icons.Filled.Settings,
+            unselectedIcon = Icons.Outlined.Settings,
+        ),
+    )
 
-                val article = Article(
-                    source = Source(
-                        id = "Test Deatil Page Image",
-                        name = "Business Insider"
-                    ),
-                    author = "Sumanth MSV",
-                    content= "Just type in the prompt of what you want — an email that asks for a full refund — hit create, and a full draft appears. It conveniently pulls in flight details from the previous email. It looks pretty close to what you want to send, but maybe you want to refine it further. In this case, a more elaborate email might increase the chances of getting the refund. “Help me write” will start rolling out as part of our Workspace updates. And just like with Smart Compose, you’ll see it get better over time.Since the early days of Street View, AI has stitched together billions of panoramic images, so people can explore the world from their device. At last year’s I/O we introduced Immersive View, which uses AI to create a high-fidelity representation of a place, so you can experience it before you visit.\n" +
-                            "\n" +
-                            "Now, we’re expanding that same technology to do what Maps does best: help you get where you want to go. Google Maps provides 20 billion kilometers of directions, every day — that’s a lot of trips. Now imagine if you could see your whole trip in advance. With Immersive View for routes you can, whether you're walking, cycling or driving.Of course, we want you to do more than just search photos — we also want to help you make them better. In fact, every month, 1.7 billion images are edited in Google Photos. AI advancements give us more powerful ways to do this. For example, Magic Eraser, launched first on Pixel, uses AI-powered computational photography to remove unwanted distractions. And later this year, using a combination of semantic understanding and generative AI, you can do much more with a new experience called Magic Editor.",
-                    description = "",
-                    publishedAt = "2024-05-01T15:06:39Z",
-                    title = "Google I/O 2023: Making AI more helpful for everyone",
-                    url = "https://www.businessinsider.com/mike-tyson-sued-for-punching-passenger-jetblue-flight-california-court-2024-5",
-                    urlToImage = "https://raw.githubusercontent.com/avanisoam/NordicNews/main/MockImages/Mock_DetailImg.png",
-                )
-                val json = Uri.encode(Gson().toJson(article))
-                navController.navigate("detail/$json")
-
-                //navController.navigate(DetailDestination.route)
-
-            }) {
-                Icon(Icons.Default.Add, contentDescription = "Add")
-            }
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+        val scope = rememberCoroutineScope()
+        var selectedItemIndex by rememberSaveable {
+            mutableStateOf(0)
         }
-    ) { innerPadding ->
-        LazyColumn(
-            modifier = Modifier
-                //.verticalScroll(rememberScrollState())
-                .padding(innerPadding),
-            //verticalArrangement = Arrangement.spacedBy(16.dp),
-        ) {
-            //Text("Your text here")
-            item {
-            FixedHeader(article = ArticleMockData.articleList[13])
-           }
-            item { Spacer(modifier = Modifier.height(50.dp)) }
-            item {
-                HorizontalCardListWithText(
-                    onItemClick = {navigateToDetailScreen(it)},
-                    articles = uiState.ArticleList.take(20)
-                )
-            }
-
-            item { Spacer(modifier = Modifier.height(50.dp)) }
-
-            item {
-                ColorfulTabsList()
-            }
-            item { Spacer(modifier = Modifier.height(50.dp)) }
-
-            item{
-                Column(modifier = Modifier.padding(
-                    start = 25.dp,
-                    end = 25.dp,
-                    bottom = 50.dp
-                )) {
-                    Text(
-                        text = "All Articles",
-                        fontWeight = FontWeight(700),
-                        fontSize = 20.sp,
-                        lineHeight = 28.sp,
-                        style = MaterialTheme.typography.headlineMedium,
-                        color = Color(29, 27, 32),
-                        modifier = Modifier.padding(
-                            //start = 25.dp,
-                            //end = 25.dp,
-                            bottom = 20.dp
+        ModalNavigationDrawer(
+            drawerContent = {
+                ModalDrawerSheet {
+                    Spacer(modifier = Modifier.height(16.dp))
+                    items.forEachIndexed { index, item ->
+                        NavigationDrawerItem(
+                            label = {
+                                Text(text = item.title)
+                            },
+                            selected = index == selectedItemIndex,
+                            onClick = {
+//                                            navController.navigate(item.route)
+                                selectedItemIndex = index
+                                scope.launch {
+                                    drawerState.close()
+                                }
+                            },
+                            icon = {
+                                Icon(
+                                    imageVector = if (index == selectedItemIndex) {
+                                        item.selectedIcon
+                                    } else item.unselectedIcon,
+                                    contentDescription = item.title
+                                )
+                            },
+                            badge = {
+                                item.badgeCount?.let {
+                                    Text(text = item.badgeCount.toString())
+                                }
+                            },
+                            modifier = Modifier
+                                .padding(NavigationDrawerItemDefaults.ItemPadding)
                         )
+                    }
+                }
+            },
+            drawerState = drawerState
+        ) {
+            Scaffold(
+                topBar = {
+                    CenterAlignedTopAppBar(
+                        title = {
+                            Text(text = "Home")
+                        },
+                        navigationIcon = {
+                            IconButton(onClick = {
+                                scope.launch {
+                                    drawerState.open()
+                                }
+                            }) {
+                                Icon(
+                                    imageVector = Icons.Default.Settings,
+                                    contentDescription = "Settings"
+                                )
+                            }
+                        }
                     )
+                },
+                bottomBar = {
+                    BottomAppBar(
+                        containerColor = Color.White,//MaterialTheme.colorScheme.primaryContainer,
+                        //contentColor = Color.Yellow,//MaterialTheme.colorScheme.primary,
+                    ) {
+                        BottomNavigationBar(navController)
+                    }
+                },
+                floatingActionButton = {
+                    FloatingActionButton(onClick = {
 
-                    ArticleListV1(
-                        onItemClick = { navigateToDetailScreen(it) },
-                        // Mock Article Data
-                        //articles = ArticleMockData.articleList
+                        val article = Article(
+                            source = Source(
+                                id = "Test Deatil Page Image",
+                                name = "Business Insider"
+                            ),
+                            author = "Sumanth MSV",
+                            content= "Just type in the prompt of what you want — an email that asks for a full refund — hit create, and a full draft appears. It conveniently pulls in flight details from the previous email. It looks pretty close to what you want to send, but maybe you want to refine it further. In this case, a more elaborate email might increase the chances of getting the refund. “Help me write” will start rolling out as part of our Workspace updates. And just like with Smart Compose, you’ll see it get better over time.Since the early days of Street View, AI has stitched together billions of panoramic images, so people can explore the world from their device. At last year’s I/O we introduced Immersive View, which uses AI to create a high-fidelity representation of a place, so you can experience it before you visit.\n" +
+                                    "\n" +
+                                    "Now, we’re expanding that same technology to do what Maps does best: help you get where you want to go. Google Maps provides 20 billion kilometers of directions, every day — that’s a lot of trips. Now imagine if you could see your whole trip in advance. With Immersive View for routes you can, whether you're walking, cycling or driving.Of course, we want you to do more than just search photos — we also want to help you make them better. In fact, every month, 1.7 billion images are edited in Google Photos. AI advancements give us more powerful ways to do this. For example, Magic Eraser, launched first on Pixel, uses AI-powered computational photography to remove unwanted distractions. And later this year, using a combination of semantic understanding and generative AI, you can do much more with a new experience called Magic Editor.",
+                            description = "",
+                            publishedAt = "2024-05-01T15:06:39Z",
+                            title = "Google I/O 2023: Making AI more helpful for everyone",
+                            url = "https://www.businessinsider.com/mike-tyson-sued-for-punching-passenger-jetblue-flight-california-court-2024-5",
+                            urlToImage = "https://raw.githubusercontent.com/avanisoam/NordicNews/main/MockImages/Mock_DetailImg.png",
+                        )
+                        val json = Uri.encode(Gson().toJson(article))
+                        navController.navigate("detail/$json")
 
-                        // Data from Api
-                        articles = uiState.ArticleList,
-                        //modifier = Modifier.padding(bottom = 20.dp)
-                    )
+                        //navController.navigate(DetailDestination.route)
+
+                    }) {
+                        Icon(Icons.Default.Add, contentDescription = "Add")
+                    }
+                }
+            ) { innerPadding ->
+                LazyColumn(
+                    modifier = Modifier
+                        //.verticalScroll(rememberScrollState())
+                        .padding(innerPadding),
+                    //verticalArrangement = Arrangement.spacedBy(16.dp),
+                ) {
+                    //Text("Your text here")
+                    item {
+                        FixedHeader(article = ArticleMockData.articleList[13])
+                    }
+                    item { Spacer(modifier = Modifier.height(50.dp)) }
+                    item {
+                        HorizontalCardListWithText(
+                            onItemClick = {navigateToDetailScreen(it)},
+                            articles = uiState.ArticleList.take(20)
+                        )
+                    }
+
+                    item { Spacer(modifier = Modifier.height(50.dp)) }
+
+                    item {
+                        ColorfulTabsList()
+                    }
+                    item { Spacer(modifier = Modifier.height(50.dp)) }
+
+                    item{
+                        Column(modifier = Modifier.padding(
+                            start = 25.dp,
+                            end = 25.dp,
+                            bottom = 50.dp
+                        )) {
+                            Text(
+                                text = "All Articles",
+                                fontWeight = FontWeight(700),
+                                fontSize = 20.sp,
+                                lineHeight = 28.sp,
+                                style = MaterialTheme.typography.headlineMedium,
+                                color = Color(29, 27, 32),
+                                modifier = Modifier.padding(
+                                    //start = 25.dp,
+                                    //end = 25.dp,
+                                    bottom = 20.dp
+                                )
+                            )
+
+                            ArticleListV1(
+                                onItemClick = { navigateToDetailScreen(it) },
+                                // Mock Article Data
+                                //articles = ArticleMockData.articleList
+
+                                // Data from Api
+                                articles = uiState.ArticleList,
+                                //modifier = Modifier.padding(bottom = 20.dp)
+                            )
+                        }
+                    }
+
                 }
             }
-
         }
     }
 }
