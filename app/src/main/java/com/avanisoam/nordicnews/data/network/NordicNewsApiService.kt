6df3ec8c6f2cf628f84d260fdeb9f961a3a5fd55
Avani Tanwar <avanisoam@gmail.com>
2025-01-19 18:32:55 +0100

    Updated Readme file

--- app/src/main/java/com/avanisoam/nordicnews/data/network/NordicNewsApiService.kt
+++ app/src/main/java/com/avanisoam/nordicnews/data/network/NordicNewsApiService.kt
@@ -5,8 +5,6 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 import java.time.LocalDate
 
-private const val API_KEY = "d6478f2cd0f34abbbe54626f658b33bd"
-
 interface NordicNewsApiService {
     @GET("ping")
     suspend fun ping(): String
@@ -15,7 +13,7 @@ interface NordicNewsApiService {
     suspend fun getNewsBySource(
         @Query("sources") sources: String,
         @Query("page") page: Int,
-        @Query("apiKey") apiKey: String = API_KEY
+        @Query("apiKey") apiKey: String = Constants.API_KEY
     ): NewsResponse
 
     @GET("everything?sortBy=popularity")
@@ -23,39 +21,39 @@ interface NordicNewsApiService {
         @Query("q") query : String,
         @Query("from") from : LocalDate,
         @Query("to") to : LocalDate?,
-        @Query("apiKey") apiKey: String = API_KEY
+        @Query("apiKey") apiKey: String = Constants.API_KEY
     ): NewsResponse
 
     @GET("everything?sortBy=publishedAt")
     suspend fun searchNewsV2(
         @Query("q") query : String,
         @Query("from") from : LocalDate?,
-        @Query("apiKey") apiKey: String = API_KEY
+        @Query("apiKey") apiKey: String = Constants.API_KEY
     ): NewsResponse
 
     @GET("everything")
     suspend fun searchNewsByDomain(
         @Query("domains") domains : String,
-        @Query("apiKey") apiKey: String = API_KEY
+        @Query("apiKey") apiKey: String = Constants.API_KEY
     ): NewsResponse
 
     @GET("top-headlines")
     suspend fun getHeadlinesByCountryAndCategory(
         @Query("country") country: String = Constants.COUNTRY,
         @Query("category") category: String,
-        @Query("apiKey") apiKey: String = API_KEY
+        @Query("apiKey") apiKey: String = Constants.API_KEY
     ): NewsResponse
 
     @GET("top-headlines?sortBy=publishedAt&pageSize=50")
     suspend fun getHeadlinesByCountryAndCategoryV1(
         @Query("category") category: String,
         @Query("country") country: String = Constants.COUNTRY,
-        @Query("apiKey") apiKey: String = API_KEY
+        @Query("apiKey") apiKey: String = Constants.API_KEY
     ): NewsResponse
 
     @GET("top-headlines")
     suspend fun getHeadlinesBySource(
         @Query("sources") sources: String,
-        @Query("apiKey") apiKey: String = API_KEY
+        @Query("apiKey") apiKey: String = Constants.API_KEY
     ): NewsResponse
 }
